b5c722e22e2e147df03a15a79a617236ff7cfc1d
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-21 10:33:48 +0800

    refactor

--- sherpa-onnx/build_386.go
+++
@@ -1,6 +0,0 @@
-//go:build windows && 386
-
-package sherpa_onnx
-
-// #cgo LDFLAGS: -L ${SRCDIR}/../lib/i686-pc-windows-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime
-import "C"
--- sherpa-onnx/build_amd64.go
+++
@@ -1,6 +0,0 @@
-//go:build windows && amd64
-
-package sherpa_onnx
-
-// #cgo LDFLAGS: -L ${SRCDIR}/../lib/x86_64-pc-windows-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime
-import "C"
