ee8b9f02bf56320e7c32483e0e27dddfc2767ec5
Fangjun Kuang <csukuangfj@gmail.com>
2023-10-21 15:16:54 +0800

    add v1.8.4

--- build_amd64.go
+++ build_amd64.go
@@ -2,5 +2,5 @@
 
 package sherpa_onnx
 
-// #cgo LDFLAGS: -L ${SRCDIR}/lib/x86_64-pc-windows-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime
+// #cgo LDFLAGS: -L ${SRCDIR}/lib/x86_64-pc-windows-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime -lkaldi-decoder-core -lsherpa-onnx-kaldifst-core -lsherpa-onnx-fst
 import "C"
