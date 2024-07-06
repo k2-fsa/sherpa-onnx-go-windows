12a83dcfafadd2721707f73953672889dca4f279
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-06 14:05:45 +0800

    remove extra files

--- build_386.go
+++
@@ -1,6 +0,0 @@
-//go:build windows && 386
-
-package sherpa_onnx
-
-// #cgo LDFLAGS: -L ${SRCDIR}/lib/i686-pc-windows-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core  -lkaldi-decoder-core -lsherpa-onnx-kaldifst-core -lsherpa-onnx-fstfar -lsherpa-onnx-fst -lpiper_phonemize -lespeak-ng -lucd -lonnxruntime -lssentencepiece_core
-import "C"
